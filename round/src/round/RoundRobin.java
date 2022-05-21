0586a5f5c40c74f267b94f88f523d18859da30b1
LeonardodeAraujoAlves <78648608+LeonardodeAraujoAlves@users.noreply.github.com>
2022-05-21 11:33:57 -0300

    Add files via upload
    algoritmo de escalonamento round robin

---
+++ round/src/round/RoundRobin.java
@@ -0,0 +1,17 @@
+package round;
+
+public class RoundRobin  {  
+	
+	public static void main(String args[])  {  
+		
+		//TrabalhoMaisCurto sjf = new TrabalhoMaisCurto();
+		escalonadorRoundRobin RR = new escalonadorRoundRobin();
+		
+		System.out.println("Selecione o escalonador que deseja");
+		RR.roundRobin();
+		
+		//sjf.TrabalhoMaisCurtoPrimeiro();
+		
+	} 
+		 
+}
